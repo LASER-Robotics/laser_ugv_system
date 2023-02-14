40887d9318c81dda6cd419c1b0daed8ec7823519
Augusto Vinicius <augustoviniciud@gmail.com>
2023-02-14 20:23:43 -0300

    Refatorado launcher do mapa, consertado fisica das rodas do omnidirecional, consertado sh para instalação de ferramentas necessárias

--- setup_enviroment.sh
+++ setup_enviroment.sh
@@ -1,39 +1,16 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
-export LANG=en_US.UTF-8
-
-sudo apt install software-properties-common
-sudo add-apt-repository universe
-
-sudo apt update && sudo apt install curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
-
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
-
-sudo apt update
-
-sudo apt upgrade
-
-sudo apt -y install ros-humble-desktop
-
-sudo apt -y install ros-dev-tools
+curl -sSL http://get.gazebosim.org | sh
 
 sudo apt -y install ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-gazebo-ros2-control
 
-curl -sSL http://get.gazebosim.org | sh
-
-echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
-
-echo "source /ugv_system/install/local_setup.bash" >> ~/.bashrc
-
 echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
 
-cd ~/ugv_system
+cd ..
 
-rosdep install --from-paths src --ignore-src -r -y
+cd ..
+
+rosdep install --from-paths src --rosdistro humble -y
 
 colcon build
 
--- ugv_simulation/launch/world_launch.py
+++ ugv_simulation/launch/world_launch.py
@@ -1,36 +1,25 @@
 import os
 
 from ament_index_python.packages import get_package_share_path
-from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import ExecuteProcess
 from launch.substitutions import LaunchConfiguration
-from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_prefix
-
-from launch_ros.actions import Node
 
 def generate_launch_description():
     ugv_simulation_path = get_package_share_path("ugv_simulation")
     world_gazebo_path = ugv_simulation_path / "worlds/laser.world"
-    
-    # We get the whole install dir
-    # We do this to avoid having to copy or softlink manually the packages so that gazebo can find them
-    description_package_name = "ugv_simulation"
-    install_dir = get_package_prefix(description_package_name)
-
-    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_robot_mode package
-    gazebo_models_path = os.path.join(ugv_simulation_path, 'models')
-    # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+    install_dir = get_package_prefix("ugv_simulation")
+    world_models_path = os.path.join(ugv_simulation_path, 'worlds/models')
 
     if 'GAZEBO_MODEL_PATH' in os.environ:
         os.environ['GAZEBO_MODEL_PATH'] = os.environ['GAZEBO_MODEL_PATH'] + \
-            ':' + install_dir + '/share' + ':' + gazebo_models_path
+            ':' + install_dir + '/share' + ':' + world_models_path
     else:
         os.environ['GAZEBO_MODEL_PATH'] = install_dir + \
-            "/share" + ':' + gazebo_models_path
+            "/share" + ':' + world_models_path
 
     if 'GAZEBO_PLUGIN_PATH' in os.environ:
         os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + \
@@ -41,7 +30,7 @@ def generate_launch_description():
     print("GAZEBO MODELS PATH=="+str(os.environ["GAZEBO_MODEL_PATH"]))
     print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
     
-    world_gazebo_arg = DeclareLaunchArgument(name="world", default_value=str(world_gazebo_path), description="World for spawnner oni claw")
+    world_gazebo_arg = DeclareLaunchArgument(name="world", default_value=str(world_gazebo_path), description="starts world for simulation")
     
     gazebo_launch = ExecuteProcess(
         cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',  '-s', 'libgazebo_ros_init.so', LaunchConfiguration('world')],
