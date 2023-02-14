95f10816d6e490782e5f3c30526bed277e27c28a
João Gabriel Santos <103967434+G4burieru@users.noreply.github.com>
2023-02-14 15:18:47 -0300

    Delete build directory

--- build/ugv_robots_descriptions/CMakeFiles/ugv_robots_descriptions_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ugv_robots_descriptions_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ugv_robots_descriptions_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/ugv_robots_descriptions/CMakeFiles/ugv_robots_descriptions_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ugv_robots_descriptions_uninstall.
-# This may be replaced when dependencies are built.
