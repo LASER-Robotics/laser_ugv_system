5e29df30bb2bd265eded9661692f449461948c89
G4burieru <joao.gabriel.silva.santos@academico.ufpb.br>
2023-02-26 15:04:30 -0300

    agora ta publicando os topicos so o rviz que nao ta funcionando

--- ugv_robots_descriptions/launch/l1br_description_launch.py
+++ ugv_robots_descriptions/launch/l1br_description_launch.py
@@ -4,6 +4,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition , UnlessCondition
 from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.parameter_descriptions import ParameterValue
 
 from launch_ros.actions import Node
 
@@ -19,67 +20,63 @@ def generate_launch_description():
     rviz_launch_arg = DeclareLaunchArgument(name="rviz", default_value="true", choices=["true", "false"], description="Launch rviz with basic visualization of robot")
     use_sim_time_arg = DeclareLaunchArgument(name="use_sim_time", default_value="true", choices=["true", "false"], description='Flag to enable use_sim_time')
     
+    robot_description = ParameterValue(
+   	    Command(["xacro ", LaunchConfiguration("model")]) ,
+      	value_type=str ,
+    )
 
     robot_state_publisher_node = Node(
    	 	package= "robot_state_publisher" ,
    	 	executable= "robot_state_publisher" ,
    	 	name= "robot_state_publisher" ,
-		namespace="l1br" ,
-   	 	parameters= [{"robot_description" : Command(["xacro ", LaunchConfiguration("model")])}]
+   	 	parameters= [{"robot_description" : robot_description}],
     )
 
     joint_state_publisher_node = Node(
    		package="joint_state_publisher",
-   		executable="joint_state_publisher",
-        namespace="l1br" ,
+   		executable="joint_state_publisher",       
    		condition=UnlessCondition(LaunchConfiguration("gui_joint"))
     )
 
     joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui",
-        namespace="l1br" ,
+        executable="joint_state_publisher_gui",    
         condition=IfCondition(LaunchConfiguration("gui_joint"))
     )
     
     gazebo = Node(
         package="gazebo_ros" ,
         executable="spawn_entity.py" ,
-        name="spawn_l1br" ,
-        namespace="l1br" ,
+        name="spawn_l1br" ,  
         output="screen" ,
-        arguments= ["-topic", "/l1br/robot_description", "-entity", "l1br"] ,
+        arguments= ["-topic", "/robot_description", "-entity", "l1br"] ,
     )
     
     robot_localization_node = Node(
 		package="robot_localization" ,
 		executable="ekf_node" ,
 		name="ekf_filter_node" ,
-		output="screen" ,
-		namespace="l1br" ,
+		output="screen" ,	
 		parameters=[str(get_package_share_path("ugv_simulation") / "config/ekf_l1br.yaml"), {"use_sim_time" : LaunchConfiguration("use_sim_time")}] 
 	)
     
     rviz = Node(
     	package="rviz2",
     	executable="rviz2",
-    	name="rviz2",
-        namespace="l1br" ,
+    	name="rviz2",     
     	output="screen",
     	arguments= ["-d", LaunchConfiguration("rvizconfig")],
     )
 
     diff_drive_spawner = Node(
         package="controller_manager",
-        executable="spawner",
-        namespace="l1br" ,
+        executable="spawner",       
         arguments=["diff_cont"],
     )
 
     joint_broad_spawner = Node(
         package="controller_manager",
-        executable="spawner",
-        namespace="l1br" ,
+        executable="spawner",     
         arguments=["joint_broad"],
     )
     return LaunchDescription([
