03ed8e3b4215f7196f5a3edda53fbfacc060dabd
João Gabriel Santos <103967434+G4burieru@users.noreply.github.com>
2023-02-26 09:58:04 -0300

    Update setup_enviroment.sh

--- setup_enviroment.sh
+++ setup_enviroment.sh
@@ -2,6 +2,8 @@
 
 curl -sSL http://get.gazebosim.org | sh
 
+sudo apt -y install ros-humble-gazebo-ros-pkgs
+
 sudo apt -y install ros-humble-ros2-control ros-humble-ros2-controllers ros-humble-gazebo-ros2-control
 
 echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
